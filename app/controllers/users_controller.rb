120b2c645e649572d212a257f655b27a5cc15885
Teru-1030 <166922869+Teru-1030@users.noreply.github.com>
2024-06-09 14:09:26 +0000

    [Update]アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+     redirect_to user_path(current_user.id)
+    end
     @user = User.find(params[:id])
   end
 
@@ -15,6 +19,10 @@ class UsersController < ApplicationController
   end
   
   def update
+    user = User.find(params[:id])
+   unless user.id == current_user.id
+    redirect_to user_path(current_user.id)
+   end
     @user = User.find(params[:id])
    if @user.update(user_params)
     flash[:notice] = "You have updated user successfully."
